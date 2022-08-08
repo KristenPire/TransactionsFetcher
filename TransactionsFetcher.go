5c62b96a243e00596675067ee0681b5c174d52ca
“Kristen <kristen.pire@epitech.eu>
2022-08-08 15:18:48 +0200

    feat(initial push)

---
+++ TransactionsFetcher.go
@@ -0,0 +1,164 @@
+package TransactionsFetcher
+
+import (
+	"context"
+	"fmt"
+	"math/big"
+	"strings"
+
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+// New ...
+func New(client *ethclient.Client, contract common.Address, contractABI string) (TransactionsFetcher, error) {
+	ca, err := abi.JSON(strings.NewReader(contractABI))
+	if err != nil {
+		return nil, err
+	}
+	return &transactionsFetcher{
+		client:       client,
+		contract:     contract,
+		contractABI:  ca,
+		transferLogs: make(map[common.Hash]LogTransfer),
+	}, nil
+}
+
+// FetchAll ...
+func (tf *transactionsFetcher) FetchAll() ([]*Transaction, error) {
+	lastBlockNumber, err := tf.getLastBlockNumber()
+	if err != nil {
+		return nil, err
+	}
+	if err := tf.newQuery(big.NewInt(0), lastBlockNumber); err != nil {
+		fmt.Println("Query issue")
+		return nil, err
+	}
+	if err := tf.fetchTranferLogs(common.Address{}, false); err != nil {
+		fmt.Println("error in fetchTransferLogs")
+		return nil, err
+	}
+	if err := tf.fetchTransferTransactions(0); err != nil {
+		fmt.Println("error in fetchTransferTransactions")
+		return nil, err
+	}
+	return tf.transferTxs, nil
+}
+
+// Fetch ...
+func (tf *transactionsFetcher) Fetch(q Query) ([]*Transaction, error) {
+	if err := tf.validateQuery(&q); err != nil {
+		return nil, err
+	}
+	if err := tf.newQuery(q.FromBlock, q.ToBlock); err != nil {
+		fmt.Println("Query issue")
+		return nil, err
+	}
+	if err := tf.fetchTranferLogs(q.Target, true); err != nil {
+		fmt.Println("error in fetchTransferLogs")
+		return nil, err
+	}
+	if err := tf.fetchTransferTransactions(q.Limit); err != nil {
+		fmt.Println("error in fetchTransferTransactions")
+		return nil, err
+	}
+	return tf.transferTxs, nil
+}
+
+// validateQuery ...
+func (tf transactionsFetcher) validateQuery(q *Query) error {
+	if q.FromBlock == nil {
+		q.FromBlock = big.NewInt(0)
+	}
+	if q.ToBlock == nil {
+		var err error
+		q.ToBlock, err = tf.getLastBlockNumber()
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (tf *transactionsFetcher) newQuery(from *big.Int, to *big.Int) error {
+	query := ethereum.FilterQuery{
+		FromBlock: from,
+		ToBlock:   to,
+		Addresses: []common.Address{
+			tf.contract,
+		},
+	}
+	var err error
+	tf.logs, err = tf.client.FilterLogs(context.Background(), query)
+	return err
+}
+
+func (tf *transactionsFetcher) fetchTranferLogs(target common.Address, withTarget bool) error {
+	topicSignature, topicName := ERC20.Transfer()
+	for _, vLog := range tf.logs {
+		switch vLog.Topics[0].Hex() {
+		case topicSignature:
+			//Topic specific code
+			var transferEvent LogTransfer
+			if err := tf.contractABI.UnpackIntoInterface(&transferEvent, topicName, vLog.Data); err != nil {
+				return err
+			}
+			transferEvent.From = common.HexToAddress(vLog.Topics[1].Hex())
+			transferEvent.To = common.HexToAddress(vLog.Topics[2].Hex())
+			//End of Topic specific code
+			if withTarget && tf.isRelated(transferEvent, target) == false {
+				continue
+			}
+			tf.transferLogs[vLog.TxHash] = transferEvent
+		}
+	}
+	return nil
+}
+
+func (tf *transactionsFetcher) fetchTransferTransactions(limit uint64) error {
+	fetched := uint64(0)
+	for txID, _ := range tf.transferLogs {
+		tx, _, err := tf.client.TransactionByHash(context.Background(), txID)
+		if err != nil {
+			return err
+		}
+		tf.transferTxs = append(tf.transferTxs, tf.toTransaction(tx))
+		fetched++
+		if limit > 0 && fetched >= limit {
+			return nil
+		}
+	}
+	return nil
+}
+
+func (tf *transactionsFetcher) toTransaction(tx *types.Transaction) *Transaction {
+	raw, _ := tx.MarshalJSON() // Don't let it like this
+	//	e18 := big.NewInt(int64(10000000000000000))
+	return &Transaction{
+		Id:           tx.Hash(),
+		TxHash:       tx.Hash(),
+		TokenAddress: tf.contract,
+		ToAddress:    tf.transferLogs[tx.Hash()].To,
+		FromAddress:  tf.transferLogs[tx.Hash()].From,
+		Kind:         "transfer",
+		Amount:       tf.transferLogs[tx.Hash()].Tokens.String(),
+		//	Amount: new(big.Int).Div(tf.transferLogs[tx.Hash()].Tokens, e18).String(),
+		Currency: "EURE",
+		Raw:      string(raw),
+	}
+}
+
+func (tf transactionsFetcher) isRelated(te LogTransfer, t common.Address) bool {
+	return te.From == t || te.To == t
+}
+
+func (tf transactionsFetcher) getLastBlockNumber() (*big.Int, error) {
+	header, err := tf.client.HeaderByNumber(context.Background(), nil)
+	if err != nil {
+		return nil, err
+	}
+	return header.Number, nil
+}
---
+++ types.go
@@ -0,0 +1,54 @@
+package TransactionsFetcher
+
+import (
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+type TransactionsFetcher interface {
+	FetchAll() ([]*Transaction, error)
+	Fetch(Query) ([]*Transaction, error)
+}
+
+type Query struct {
+	FromBlock *big.Int
+	ToBlock   *big.Int
+	Target    common.Address
+	Limit     uint64
+}
+
+type LogTransfer struct {
+	From   common.Address
+	To     common.Address
+	Tokens *big.Int
+}
+
+type Transaction struct {
+	Id           common.Hash
+	TxHash       common.Hash
+	Address      common.Address // ?
+	TokenAddress common.Address
+	BlockNumber  big.Int
+	BlockHash    common.Hash
+	ToAddress    common.Address
+	FromAddress  common.Address
+	Kind         string
+	Amount       string
+	Currency     string
+	Raw          string
+}
+
+type transactionsFetcher struct {
+	client      *ethclient.Client
+	contract    common.Address
+	target      common.Address
+	contractABI abi.ABI
+
+	logs         []types.Log
+	transferLogs map[common.Hash]LogTransfer
+	transferTxs  []*Transaction
+}
